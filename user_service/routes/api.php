0d710684e04b8fedafb8899e1d98b0ed15a47913
Manh Pham <manhpham90vn@icloud.com>
2025-01-19 01:43:39 +0700

    add reverse proxy configuration with Nginx; enhance Docker healthchecks and update database connection settings

--- user_service/routes/api.php
+++ user_service/routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 
 Route::prefix('user_service/v1')->group(function () {
+    Route::get('/health', function () {
+        DB::select('SELECT 1');
+        return response()->json(['status' => 'ok']);
+    });
     Route::post('/profiles', [ProfileController::class, 'storeOrUpdate']);
     Route::get('/profiles/{id}', [ProfileController::class, 'show']);
 });
