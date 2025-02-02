1fecf468364956fc24b3b1b42e73aab173201680
Manh Pham <manhpham90vn@icloud.com>
2025-02-02 14:36:57 +0700

    add product service endpoints and validation; implement product DTO and update Docker configuration

--- product_service/src/app.controller.ts
+++ product_service/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateProductDto } from './dtos/createProduct';
 
 @Controller('/api/product_service/v1')
 export class AppController {
@@ -8,5 +9,25 @@ export class AppController {
   @Get('/health')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/products')
+  getProducts(): string {
+    return '';
+  }
+
+  @Get('/products/:id')
+  getProductById(@Param('id') id: string): string {
+    return id;
+  }
+
+  @Post('/products')
+  createProduct(@Body() body: CreateProductDto): string {
+    return body.name;
+  }
+
+  @Post('/products/:id')
+  updateProduct(@Param('id') id: string): string {
+    return id;
   }
 }
